2e97604e9f8540428243d46f37fdee6258c4d4c4
xiewei <xiewei@finebi.com>
2020-06-02 00:19:58 +0800

    简化配置

--- src/main/java/com/example/xywx/model/Platform.java
+++ src/main/java/com/example/xywx/model/Platform.java
@@ -20,16 +20,4 @@ public class Platform implements Serializable {
     private String createBy;
     private List<PlatformMenu> platformMenuList;
 
-    private String test1;       //测试1·
-    private String test2;       //我偏要改
-    private String test3;       //测试3·
-    private String test4;       //我才是真正的测试4
-
-    @Override
-    public String toString() {
-        return "Platform{" +
-                "test1='" + test1 + '\'' +
-                ", test3='" + test3 + '\'' +
-                '}';
-    }
 }
