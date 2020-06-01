5e08f2e8d10075f972e4854dfad196b172d51d6b
谢玮 <xiewei@finebi.com>
2020-06-01 22:15:55 +0800

    合并后提交

--- src/main/java/com/example/xywx/model/Platform.java
+++ src/main/java/com/example/xywx/model/Platform.java
@@ -21,7 +21,7 @@ public class Platform implements Serializable {
     private List<PlatformMenu> platformMenuList;
 
     private String test1;       //测试1·
-    private String test2;       //是对的，不要改
+    private String test2;       //我偏要改
     private String test3;       //测试3·
     private String test4;       //我才是真正的测试4
 
