bdddff017d831154082eda1fd1fb983d0ee12c91
AA51890 <119403932@qq.com>
2020-06-01 19:07:35 +0800

    第一次创建

---
+++ src/main/java/com/example/xywx/XywxApplication.java
@@ -0,0 +1,13 @@
+package com.example.xywx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XywxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XywxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/xywx/XywxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xywx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XywxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
